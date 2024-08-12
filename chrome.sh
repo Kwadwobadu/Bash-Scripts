a61f6da7fb9deb81b87cfcb10d38bf99ee4ed6b9
Kwadwo Badu <k.badu329@gmail.com>
2024-08-12 11:19:01 +0100

    Update chrome.sh
    Made changes to the code so that the script will run as the logged in user since on Jamf its running the script as Root which is causing problems when opening Chrome.

--- chrome.sh
+++ chrome.sh
@@ -2,15 +2,21 @@
 
 
 # Script that will donwload & install Google Chrome
+# Version 2.0 - added a code whereby it will switch the logged in user and run the code.
+
+
 
 URL="https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
 DOWNLOAD_DIR="/var/tmp"
 MOUNT_POINT="/Volumes/googlechrome.dmg"
 
 
+killall="/usr/bin/killall"
+loggedInUser=$( ls -l /dev/console | awk '{print $3}' )
+
 # Set directory to DOWNLOAD_DIR = /var/tmp
 
-cd "$DOWNLOAD_DIR" || { echo "Failed to change directory to $DOWNLOAD_DIR"; exit 1; }
+sudo -u $loggedInUser cd "$DOWNLOAD_DIR" || { echo "Failed to change directory to $DOWNLOAD_DIR"; exit 1; }
 
 curl -O --verbose "$URL"
 
@@ -21,7 +27,7 @@ fi
 
 # Mounts the Google Chrome dmg
 
-hdiutil attach googlechrome.dmg -mountpoint $MOUNT_POINT
+sudo -u $loggedInUser hdiutil attach googlechrome.dmg -mountpoint $MOUNT_POINT
 if [ $? -ne 0 ]; then
     echo "Failed to mount $$FILE_NAME"
     exit 1
@@ -29,7 +35,7 @@ fi
 
 # Copies Google Chrome.app to Applications folder
 
- cp -R /Volumes/googlechrome.dmg/Google\ Chrome.app  /Applications
+sudo -u $loggedInUser cp -R /Volumes/googlechrome.dmg/Google\ Chrome.app  /Applications
  if [ $? -ne 0 ]; then
     echo "Failed to copy Google Chrome"
 else
@@ -47,7 +53,7 @@ sleep 1
 
 # Ejects the dmg
 
-hdiutil detach $MOUNT_POINT
+sudo -u $loggedInUser hdiutil detach $MOUNT_POINT
 
 sleep  1
 
