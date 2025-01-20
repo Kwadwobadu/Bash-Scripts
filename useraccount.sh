6d2d65e9bae8f39cd7241ce1aac4ead4d2c17b5b
Kwadwo <k.badu329@gmail.com>
2025-01-20 23:18:34 +0000

    Update useraccount.sh

--- useraccount.sh
+++ useraccount.sh
@@ -1,25 +1,41 @@
 #!/bin/bash
 
+#V2 properly wrote the script properly as the first verison the logic didn't make sense
+#######################################################################
+# if [[ $? -eq 0 ]]; then
+#        echo "User $username and their home directory have been deleted."
+#    elif
+#    
+#        echo "Failed to delete the user $username."
+#    then
+#    
+#       echo "Error: User $username does not exist."
+#
+#fi
+########################################################################
+
+
+if [ "$EUID" -ne 0 ]; then
+	echo "This script must be run as root, exiting..."
+	exit 1
+
+fi
+
 # Ask for the username to delete
-read -p "Enter the username you want to delete: " username
+read -r -p "Enter the username you want to delete: " username
 
 # Check if the user exists if not exit the script
 if id "$username" &>/dev/null; then
     echo "User $username found. Proceeding to delete the account..."
+
+    #Linux deletion command
+    #sudo userdel -r "$username"
+
+    #Mac deletion command
+    sudo sysadminctl -deleteUser "$username"
+
 else
     echo "User $username is not found exiting"
     fi
 
-exit 1
-
-    # Delete the user and their home directory
-    sudo sysadminctl -deleteUser "$username"
-
-    if [[ $? -eq 0 ]]; then
-        echo "User $username and their home directory have been deleted."
-    else
-        echo "Failed to delete the user $username."
-    fi
-else
-    echo "Error: User $username does not exist."
-fi
+exit 0
