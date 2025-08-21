4152e99f3ec50691eb005877abf8d5d41cc1cd55
Kwadwo Badu <k.badu329@gmail.com>
2025-08-21 11:38:53 +0100

    Update Nudge_Kill.sh

--- Nudge_Kill.sh
+++ Nudge_Kill.sh
@@ -2,6 +2,8 @@
 # filepath: untitled:Untitled-2
 
 
+# This script will find all instances of Nudge proceess & it will kill the process using arg -9
+
 
 # Find the PID(s) of Google Chrome
 pids=$(pgrep "Nudge")
